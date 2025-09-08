20bc1ac6c53deab98bd7e86bfb308736e2f31995
faddea <francisco777smith@gmail.com>
2025-09-08 13:12:19 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Espera 3 segundos (3000 milisegundos) y redirige
+// Espera 3 segundos y redirige
 setTimeout(() => {
-  window.location.href = "menu.html"; // Cambia esto por la URL o ruta correcta
+    window.location.href = "menu.html"; // Vuelve a elegir la materia
 }, 3000);
